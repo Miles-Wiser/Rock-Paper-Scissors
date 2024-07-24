5863a24638db0976128051167b80bed208fac3b8
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2024-07-24 08:23:45 -0600

    Update rock-paper-scissors.cpp

--- rock-paper-scissors.cpp
+++ rock-paper-scissors.cpp
@@ -1,23 +1,33 @@
 
-// Online C++ compiler to run C++ program online
+// https://www.programiz.com/cpp-programming/online-compiler/
 
 #include <iostream>
-#include <string>
-#include <cstdlib>
+#include <cstdlib> // maybe use srand() or rand()?
 
 int main() {
     
-    std::cout << "Choose rock, paper, or scissor: ";
+    std::cout << "Choose 1 for rock,  2 for paper, or 3 for scissor: ";
     
-    //std::string user_option;
-    //std::cin >> user_option;
+    int user_option;
+    while(1 >= user_option ) {
+        std::cout // unfinshed loop to check for user_option between 1 and 3
+    }
+    std::cin >> user_option;
     
-    int ub = 3, lb = 1;
-    for (int i = 0; i < 5; i++) 
-        std::cout << (std::rand() % (ub - lb + 1)) + lb << " ";
+    int comp_option = 3; //Figure out how to make this random
     
-    //std::cout << user_option << std::endl;
-    //std::cout << comp_option << std::endl;
+    switch (comp_option) {
+        case 1:
+            std::cout << "I choose Rock\n";
+            break;
+        case 2:
+            std::cout << "I choose Paper\n";
+            break;
+        case 3:
+            std::cout << "I choose Scissors\n";
+            break;
+    }
+
     
     return 0;
 }
