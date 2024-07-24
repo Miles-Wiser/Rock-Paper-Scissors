ce78e181934af79a988de1c3de3f14d45d08eb14
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2024-07-24 10:13:42 -0600

    Update rock-paper-scissors.cpp

--- rock-paper-scissors.cpp
+++ rock-paper-scissors.cpp
@@ -9,10 +9,24 @@ int main() {
     std::cout << "Choose 1 for rock,  2 for paper, or 3 for scissor: ";
     
     int user_option;
-    while(1 >= user_option ) {
-        std::cout // unfinshed loop to check for user_option between 1 and 3
-    }
     std::cin >> user_option;
+    while(1 > user_option or user_option > 3) {
+        std::cout << "That option is invalid.\n";
+        std::cout << "Choose 1 for rock,  2 for paper, or 3 for scissor: ";
+        std::cin >> user_option;
+    }
+    
+    switch (user_option) {
+        case 1:
+            std::cout << "You chose Rock.\n";
+            break;
+        case 2:
+            std::cout << "You chose Paper.\n";
+            break;
+        case 3:
+            std::cout << "You chose Scissors.\n";
+            break;
+    }
     
     int comp_option = 3; //Figure out how to make this random
     
@@ -27,7 +41,6 @@ int main() {
             std::cout << "I choose Scissors\n";
             break;
     }
-
     
     return 0;
 }
